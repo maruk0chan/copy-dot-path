63a90507d6ee86c09121ebefc3b62914553b02b3
Marco Chan <marcochan94@hotmail.com>
2025-01-12 18:51:11 +0800

    chore: update command

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,7 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
-    "js-dot-path.getJavascriptDotNotationPath",
+    "js-dot-path.getDotNotationPath",
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
