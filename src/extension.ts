6e295a5d28c9d0fd9e39489b0a265ad857926d4c
Marco Chan <marcochan94@hotmail.com>
2025-01-12 18:52:24 +0800

    change: extension name to copy-dot-path

--- src/extension.ts
+++ src/extension.ts
@@ -3,17 +3,17 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "js-dot-path" is now active!');
+  console.log('Congratulations, your extension "copy-dot-path" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
-    "js-dot-path.getDotNotationPath",
+    "copy-dot-path.getDotNotationPath",
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from js-dot-path!");
+      vscode.window.showInformationMessage("Hello World from copy-dot-path!");
     }
   );
 
